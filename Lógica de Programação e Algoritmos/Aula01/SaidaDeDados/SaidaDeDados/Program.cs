21036e7e5bf27be9c31141f7784c2eb7674a3369
nargaoff <gabrielpironato@gmail.com>
2026-07-17 21:51:26 -0300

    finalizado aula 1 dia 17/07/2026

---
+++ Lógica de Programação e Algoritmos/Aula01/SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+int numero = 12;
+double flutuante = 2.85;
+string palavra = "SENAI";
+char caracter = 'S';
+bool logica1 = true;
+bool logica2 = false;
+
+WriteLine("Hello, World!");
+ReadKey();
