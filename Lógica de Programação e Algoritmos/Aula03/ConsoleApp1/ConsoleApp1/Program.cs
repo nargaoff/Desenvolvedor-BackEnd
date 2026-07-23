a6e09ca3d2cf1f1b1ac57c3b1411c52921f88c0b
nargaoff <gabrielpironato@gmail.com>
2026-07-22 21:55:21 -0300

    Medidas

---
+++ Lógica de Programação e Algoritmos/Aula03/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+try
+{
+    //Declaração de variáveis
+    int a, b, c;
+    double area_q, area_t, area_tp;
+
+    //entrada de dados
+    Write("Digite o valor de A: ");
+    a = int.Parse(ReadLine());
+    Write("Digite o valor de B: ");
+    b = int.Parse(ReadLine());
+    Write("Digite o valor de C: ");
+    c = int.Parse(ReadLine());
+
+    //Processamento de dados
+    area_q = Math.Pow(a, 2);
+    area_t = (a * b) / 2;
+    area_tp = ((a + b) * c) / 2;
+
+    //Saida de dados
+    WriteLine($"Area do quadrado = {area_q:f4}");
+    WriteLine($"Area do triangulo = {area_t:f4}");
+    WriteLine($"Area do trapezio = {area_tp:f4}");
+}
+catch
+{
+    WriteLine("Digite um valor válido");
+}
+finally
+{
+    WriteLine("Encerrando o programa");
+}
