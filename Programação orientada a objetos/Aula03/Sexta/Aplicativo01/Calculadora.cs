b1c3b3ee5c3d40c4416a13590e00100795b5e92a
nargaoff <gabrielpironato@gmail.com>
2026-08-14 19:48:49 -0300

    Versão2

---
+++ Programação orientada a objetos/Aula03/Sexta/Aplicativo01/Calculadora.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Aplicativo01
+{
+    internal class Calculadora
+    {
+        //Membros
+        //Primeiro - Campos
+        double raio;
+        public const double pi = 3.1415;
+
+        //Segundo membro da classe - Construtor
+        public Calculadora(double raio)
+        {
+            this.raio = raio;
+        }
+        //Terceiro - Métodos
+        public double Circunferencia()
+        {
+            return 2 * pi * raio;
+        }
+
+        public double volume()
+        {
+            return (4 * pi * Math.Pow(raio, 3)) / 3;
+        }
+
+        public void PI()
+        {
+            Console.WriteLine(pi);
+        }
+
+    }
+}
--- Programação orientada a objetos/Aula03/Sexta/Aplicativo01/Program.cs
+++ Programação orientada a objetos/Aula03/Sexta/Aplicativo01/Program.cs
@@ -1,16 +1,19 @@
-﻿//Declaração de variaveis
-double raio, circunferencia, volume;
-const double pi = 3.1415;
+﻿using Aplicativo01;
+//Declaração de variaveis
+Calculadora calculadora;
+double raio;
+
 
 //Entrada de dados
 Console.Write("Entre com o valor do raio da circunferencia: ");
 raio = double.Parse(Console.ReadLine());
 
+//Instanciação do objeto
+calculadora = new Calculadora(raio);
+
 //Processamento de dados
-circunferencia = 2 * pi * raio;
-volume = (4 * pi * Math.Pow(raio, 3)) / 3;
 
 //Saida de dados
-Console.Write($"Circunferencia: {circunferencia:f2}" +
-    $"Volume: {volume:f2}" +
-    $"PI = {pi:f2}");
+Console.Write($"Circunferencia: {calculadora.Circunferencia():f2}" +
+    $"Volume: {calculadora.volume:f2}");
+calculadora.PI();
