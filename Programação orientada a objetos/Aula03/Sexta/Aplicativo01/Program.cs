a6143399b2e2fd475a94542d18840860251d4a02
nargaoff <gabrielpironato@gmail.com>
2026-08-14 19:26:11 -0300

    Versão 1

---
+++ Programação orientada a objetos/Aula03/Sexta/Aplicativo01/Program.cs
@@ -0,0 +1,16 @@
+﻿//Declaração de variaveis
+double raio, circunferencia, volume;
+const double pi = 3.1415;
+
+//Entrada de dados
+Console.Write("Entre com o valor do raio da circunferencia: ");
+raio = double.Parse(Console.ReadLine());
+
+//Processamento de dados
+circunferencia = 2 * pi * raio;
+volume = (4 * pi * Math.Pow(raio, 3)) / 3;
+
+//Saida de dados
+Console.Write($"Circunferencia: {circunferencia:f2}" +
+    $"Volume: {volume:f2}" +
+    $"PI = {pi:f2}");
---
+++ Programação orientada a objetos/Aula03/Sexta/Estrutura/aluno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estrutura
+{
+    internal struct aluno
+    {
+        //Campos
+        public string nome;
+        public int cpf;
+        public double altura;
+
+        public aluno(string nome, int cpf) : this()
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+        }
+
+        public aluno(string nome, int cpf, double altura)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.altura = altura;
+        }
+
+
+        //Metodos
+        public double Idade (int idade)
+        {
+            return idade / 365;
+        }
+
+    }
+}
