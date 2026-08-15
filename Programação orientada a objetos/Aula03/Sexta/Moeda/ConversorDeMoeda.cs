7137dc19564ddc2768160fb8626ec03ad8c04e36
nargaoff <gabrielpironato@gmail.com>
2026-08-14 21:09:38 -0300

    Conversor de Moedas

---
+++ Programação orientada a objetos/Aula03/Sexta/Moeda/ConversorDeMoeda.cs
@@ -0,0 +1,34 @@
+﻿namespace Moeda
+{
+    internal class ConversorDeMoeda
+    {
+        public double Cotacao;
+        public double Dolares;
+
+        public double Converter()
+        {
+            double valorReais = Cotacao * Dolares;
+            double iof = valorReais * 0.06;
+
+            return valorReais + iof;
+        }
+    }
+
+    internal class ConversorDeMoedas
+    {
+        static void Main()
+        {
+            ConversorDeMoeda conversor = new ConversorDeMoeda();
+
+            Console.Write("Qual é a cotação do dolar?: ");
+            conversor.Cotacao = double.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Quantos dolares voce vai comprar?: ");
+            conversor.Dolares = double.Parse(Console.ReadLine()!);
+
+            double valorFinal = conversor.Converter();
+
+            Console.WriteLine("Valor a ser pago em reais = " + valorFinal.ToString("f2"));
+        }
+    }
+}
